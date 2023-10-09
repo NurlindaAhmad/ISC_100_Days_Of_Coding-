25fb3739be783b7ebc9c48216ea5d8a66db6ebe0
NurlindaAhmad <147230095+NurlindaAhmad@users.noreply.github.com>
2023-10-09 16:57:47 +0700

    Add files via upload

---
+++ Hari1_biodata.java
@@ -0,0 +1,24 @@
+
+package Pribadi;
+
+
+public class Hari1_biodata {
+    public static void main(String[] args) {
+        String Biodata = "Biodata Diri";
+        String Nama = "Nurlinda Ahmad";
+        String  Nim = "D0222036";
+        String Ttl = "Mamuju/20-juli-2003";
+        String Prodi = "Informatika";
+        String Universitas = "Universitas Sulawesi Barat";
+        int Angkatan = 2022;
+        
+        System.out.println("biodata     :" + Biodata );
+        System.out.println("nama        :" + Nama);
+        System.out.println("nim         : " + Nim);
+        System.out.println("ttl         :" + Ttl);
+        System.out.println("prodi       :" + Prodi);
+        System.out.println("universitas : " + Universitas);
+        System.out.println("angkatan    :" + Angkatan);
+    }
+    
+}
